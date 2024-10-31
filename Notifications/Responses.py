08c6f230dfe9788742399f8499cba74dd1445a36
TrentDufour <155991710+TrentDufour@users.noreply.github.com>
2024-10-31 17:12:26 -0500

    Notification Idea Thrown out

---
+++ Notifications/Responses.py
@@ -0,0 +1,18 @@
+from random import choice, randint
+
+
+
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    '''if lowered == "":
+        return "well, you\'re awfully quiet"
+    elif "How are you?" in lowered:
+        return "Great!"
+    elif "Hello" in lowered: 
+        return "Howdy!"
+    elif "Bye" in lowered:
+        return "See ya!"
+    else: 
+        return response'''
+    
